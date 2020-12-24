d7d00e08ce990996d32a9aa524237b62afc17fcd
Jordan Harband <ljharb@gmail.com>
2020-12-24 00:18:59 -0800

    [Fix] `shim`: install `Reflect` on the global when it is not available

--- shim.js
+++ shim.js
@@ -2,20 +2,19 @@
 
 var getPolyfill = require('./polyfill');
 var define = require('define-properties');
-var globalThis = require('globalthis');
+var globalThis = require('globalthis')();
 
 module.exports = function shimReflectOwnKeys() {
 	var polyfill = getPolyfill();
 	if (typeof Reflect === 'undefined') {
 		define(globalThis, {
-			Reflect: { ownKeys: polyfill }
-		});
-	} else {
-		define(Reflect, { ownKeys: polyfill }, {
-			values: function testReflectOwnKeys() {
-				return Reflect.ownKeys !== polyfill;
-			}
+			Reflect: {}
 		});
 	}
+	define(Reflect, { ownKeys: polyfill }, {
+		values: function testReflectOwnKeys() {
+			return Reflect.ownKeys !== polyfill;
+		}
+	});
 	return polyfill;
 };
