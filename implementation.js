87268243942519ab13e99e4ec2d5589169045b52
Jordan Harband <ljharb@gmail.com>
2024-12-28 22:22:36 -0800

    [Refactor] use `own-keys`

--- implementation.js
+++ implementation.js
@@ -1,7 +1,3 @@
 'use strict';
 
-var OwnPropertyKeys = require('es-abstract/helpers/OwnPropertyKeys');
-
-module.exports = function ownKeys(o) {
-	return OwnPropertyKeys(o);
-};
+module.exports = require('own-keys');
